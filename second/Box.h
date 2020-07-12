b730e3d2deb7614683b6c5eeeba4538c080277b5
Nameless <tyrebrown873@gmail.com>
2020-07-12 17:26:02 -0400

    box

--- second/Box.h
+++ second/Box.h
@@ -9,6 +9,10 @@ class Box : public sf::RectangleShape
 public:
 	Box(const sf::Vector2f& position, const sf::Vector2f& size, b2World& world, b2BodyType type = b2_dynamicBody, sf::Color color = sf::Color::Green);
 	void Update();
+	b2Body& GetBody() const
+	{
+		return *body;
+	}
 private:
 	b2Body* body = nullptr;
 	b2Fixture* fixture = nullptr;
